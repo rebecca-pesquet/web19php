7a5b5d133ef92886e1ee2628acd6b834c64d1a02
Fabien <flierville@cesi.fr>
2020-09-22 13:52:58 +0200

    cours

---
+++ eleves.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "<p>Bonjour ".$_GET["prenom"].", tu eu un A en php</p>";
--- index.php
+++ index.php
@@ -4,6 +4,21 @@
 <?php
  //echo getXFirstWord("Voici ma belle phrase",2);
  echo get_words("Voici ma belle phrase",2);
-?>
 
+ // $_GET = Récupère info URL
+
+ // $_POST = Récupère infos Formulaire
+
+var_dump($_GET);
+var_dump($_POST);
+
+ ?>
+<p>Consulter les note de <a href="/eleves.php?prenom=David">David</a></p>
 <?php require("_baseHTML2.php"); ?>
+
+
+
+
+
+
+
