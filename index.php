f8b5b8b516e5e1568ae8eb30f0f4fed79fd8970e
Fabien <flierville@cesi.fr>
2020-09-22 14:31:30 +0200

    cours

--- index.php
+++ index.php
@@ -13,7 +13,30 @@ var_dump($_GET);
 var_dump($_POST);
 
  ?>
+
 <p>Consulter les note de <a href="/eleves.php?prenom=David">David</a></p>
+<?php
+$prenoms = ["Brice", "Julie", "Aegir", "Emilie"];
+//Exo : Créer un lien HREF pour chacun de ces prénoms qui emmène vers la page eleves, sauf pour "Aegir" (Boucle + IF)
+foreach ($prenoms as $prenom){
+    if($prenom != "Aegir"){
+        echo "<p>Consulter les note de <a href=\"/eleves.php?prenom=".$prenom."\">".$prenom."</a></p>";
+    }
+}
+
+$prenomsNote = [
+        "Brice" => "C",
+        "Julie" => "B",
+        "Aegir" => "D",
+        "Emilie" => "A"
+];
+// Exo : Du coup on va sur Eleves.php et on fait une variable aussi sur la note
+
+
+
+
+?>
+
 <?php require("_baseHTML2.php"); ?>
 
 
