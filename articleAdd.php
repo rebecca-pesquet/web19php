d4269856ef33d13c0bd9da6ea4dc1ae771f8bb59
Fabien <flierville@cesi.fr>
2020-09-23 11:35:24 +0200

    cours

--- articleAdd.php
+++ articleAdd.php
@@ -1,2 +1,15 @@
 <?php
+require ("config.php"); //Pour avoir entre autre la connexion à la BDD
 
+$requete = $bdd->prepare("INSERT INTO articles (Titre, Description, DateAjout, Auteur) VALUES(:Titre, :Description, :DateAjout, :Auteur)");
+
+$requete->execute([
+    "Titre" => "Un autre article",
+    "Description" => "Ceci est un nouvel article",
+    "DateAjout" => "2020-09-23",
+    "Auteur" => "Rebecca"
+]);
+
+$lastId = $bdd->lastInsertId();
+
+echo "<p>Bravo vous venez de créer un article dont l'ID est : ".$lastId."</p>";
