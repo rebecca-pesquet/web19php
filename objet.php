704f38482f15c9afca73e63405a71d02e56eb74a
Fabien <flierville@cesi.fr>
2020-09-24 14:16:40 +0200

    cours

--- objet.php
+++ objet.php
@@ -1,5 +1,10 @@
 <?php
 require "./myclass/Article.php";
+require "./config.php";
 use myclass\Article;
 
 $article = new Article();
+$article->setTitre("Mon article OBJET")->setDesription("C'est une super description pour cet article")->setAuteur("Mathis")->setDateAjout("2020-09-24");
+
+$sql = $article->SqlAdd($bdd);
+var_dump($sql);
