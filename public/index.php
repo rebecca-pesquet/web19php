fb75833ff6fe42e26169a1cd386cd709dc7eae24
Fabien <flierville@cesi.fr>
2020-09-24 15:47:58 +0200

    cours

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
-
+/*
+ * Autoloader de Classe
+ */
 function chargerClasse($classe){
     //Séparateur répertoire : "\" "/"
     $ds = DIRECTORY_SEPARATOR;
@@ -15,6 +17,25 @@ function chargerClasse($classe){
 }
 spl_autoload_register("chargerClasse");
 
-use src\Model\Article;
+// Ajout d'un article en Database
+$hostname="localhost";
+$username="root";
+$password="";
+$dbname="cesiblog";
 
+try
+{
+    $bdd = new PDO('mysql:host='.$hostname.';dbname='.$dbname.';charset=utf8', $username, $password);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+
+
+use src\Model\Article;
 $article = new Article();
+$article->setTitre("Le MVC c'est la vie")->setDateAjout("2020-08-24");
+$sql = $article->SqlAdd();
+
