31bb588542ecb3b080cbd472f069fe49afbec936
Fabien <flierville@cesi.fr>
2020-09-25 09:42:33 +0200

    cours

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -17,5 +17,33 @@ class ArticleController{
         echo "Ici je vais devoir aller chercher tous mes articles pour les afficher une page HTML";
     }
 
+    public function Fixtures(){
+        //Vider la table
+        /*
+        $requete = $bdd->prepare("TRUNCATE TABLE articles");
+        $requete->execute();
+        */
+
+//Tableau "Jeu de donnée"
+        $Titres = ["PHP en force", "Java en baisse", "JS un jour ça marchera", "Flutter valeur montante", "GO le futur"];
+        $Prenoms = ["Rebecca", "Alexandre", "Emilie", "Léo", "Aegir"];
+        $datedujour = new \DateTime();
+        for($i = 0;$i < 200;$i++){
+            $datedujour->modify("+1 day");
+            shuffle($Titres);
+            shuffle($Prenoms);
+
+            //Objet Article
+            $objArticle = new Article();
+            $objArticle->setTitre($Titres[0]);
+            $objArticle->setDesription("Ceci est une excellent description");
+            $objArticle->setDateAjout($datedujour->format("Y-m-d"));
+            $objArticle->setAuteur($Prenoms[0]);
+
+            //Exécuter l'insertion
+            $objArticle->SqlAdd(BDD::getInstance());
+        }
+        header("Location:/controller=Article&action=All");
+    }
 
 }
