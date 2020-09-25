11fca7e04e58d64c618e25dda6af94e3dacd65e3
Fabien <flierville@cesi.fr>
2020-09-25 11:24:56 +0200

    cours

---
+++ src/Controller/AbstractController.php
@@ -0,0 +1,24 @@
+<?php
+namespace src\Controller;
+
+class AbstractController{
+    protected $loader;
+    protected $twig;
+
+    public function getTwig(){
+        return $this->twig;
+    }
+
+    public function __construct(){
+        $this->loader = new \Twig\Loader\FilesystemLoader($_SERVER["DOCUMENT_ROOT"]."/../templates");
+        $this->twig = new \Twig\Environment(
+            $this->loader,[
+                "cache" => $_SERVER["DOCUMENT_ROOT"]."/../var/cache",
+                "debug" => true
+            ]
+        );
+        $this->twig->addExtension(new \Twig\Extension\DebugExtension());
+
+    }
+
+}
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,13 +4,14 @@ namespace src\Controller;
 use src\Model\Article;
 use src\Model\BDD;
 
-class ArticleController{
+class ArticleController extends AbstractController {
 
     public function Add(){
         echo "Ici je vais devoir faire un formulaire HTML qui me permet d'ajouter un article dans ma base de donnée";
     }
 
     public function All(){
+
         $articles = new Article();
         $datas = $articles->SqlGetAll(BDD::getInstance());
         var_dump($datas);
